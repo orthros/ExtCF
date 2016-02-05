99912d68bc8a1d944f127252651aa4dc322e93f9
orthros <colinnlsn@gmail.com>
2016-02-05 12:55:20 -0600

    Added some very basic unit tests based on the xunit framework. Using a nuget package as opposed to a lib reference.
    Consider moving the tests into their own folder/solution in the future.

---
+++ src/ExtCF.Tests/ExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using ExtCF.ContractExtensions.Factory;
+using Orth.Core.Logs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExtCF.Tests
+{
+    public class ExtensionsTests
+    {
+        [Fact]
+        public void ConstructorFailOnNulls()
+        {
+            ILog nullLog = null;
+            var returnedException = Assert.Throws(typeof(ArgumentNullException), () =>
+             {
+                 IContractExtensionFactory factory = new ContractExtensionFactory(nullLog);
+             });
+
+            Assert.IsType(typeof(ArgumentNullException), returnedException);
+
+            Assert.Equal("log", (returnedException as ArgumentNullException).ParamName);            
+        }
+
+    }
+}
---
+++ src/ExtCF.Tests/MetadataTests.cs
@@ -0,0 +1,55 @@
+﻿using ExtCF.ContractExtensions.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace ExtCF.Tests
+{
+    public class MetadataTests
+    {
+        [Fact]
+        public void TestEmptyStringParameter()
+        {
+            Assert.Throws(typeof(ArgumentNullException), () =>
+             {
+                 ContractExtensionMetadataAttribute attribute = new ContractExtensionMetadataAttribute("");
+             });
+        }
+
+        [Fact]
+        public void TestNullStringParameter()
+        {
+            Assert.Throws(typeof(ArgumentNullException), () =>
+             {
+                 ContractExtensionMetadataAttribute attribute = new ContractExtensionMetadataAttribute(null);
+             });
+        }
+
+        [Theory, MemberData("FunctionNames")]        
+        public void TestFunctionNames(string name)
+        {
+            var attribute = new ContractExtensionMetadataAttribute(name);
+            Assert.Equal(name, attribute.FunctionName);
+        }
+
+        /// <summary>
+        /// The names of the function to test for metadata
+        /// TODO: Make this read from a file
+        /// </summary>
+        public static IEnumerable<object[]> FunctionNames
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { "asdf" },
+                    new object[] { "qwerty" }
+                };
+            }
+        }
+    }
+}
