03f5b19f0180b6574c2388c4e891db4de2d3a111
orthros <colinnlsn@gmail.com>
2016-05-06 08:53:21 -0500

    Added a small unit test project for ExtCF.Hosting.csproj, ExtCF.Hosting.Tests.csproj.
    Restructured the directories a bit.

---
+++ test/ExtCF.Hosting.Tests/Stubs/StubHost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtCF.Hosting.Tests.Stubs
+{
+    internal class StubHost
+    {
+        public string MyString { get; private set; }
+        public int MyInt { get; private set; }
+
+        public StubHost(string myString, int myInt)
+        {
+            MyString = myString;
+            MyInt = myInt;
+        }
+    }
+}
---
+++ test/ExtCF.Hosting.Tests/TestInstanceProvider.cs
@@ -0,0 +1,27 @@
+﻿using ExtCF.Hosting.Tests.Stubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExtCF.Hosting.Tests
+{
+    public class TestInstanceProvider
+    {
+        [Fact]
+        public void TestConstructor()
+        {
+            string mystring = "adsf";
+            int myInt = 1;
+
+            Func<StubHost> conFunc = () => new StubHost(mystring, myInt);
+
+            var provider = new InjectInstanceProvider<StubHost>(conFunc);
+
+            var testVal = provider.GetInstance(null);
+            Assert.IsType(typeof(StubHost), testVal);            
+        }
+    }
+}
