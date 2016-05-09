6372909926f0cb4426eaed3cfbcf105c68f94123
orthros <colinnlsn@gmail.com>
2016-05-09 10:56:03 -0500

    Updated the AssemblyInfo.cs for ExtCF.Hosting to include an AssemblyDescription and AssemblyCompany

--- src/ExtCF.Hosting/Properties/AssemblyInfo.cs
+++ src/ExtCF.Hosting/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExtCF.Hosting")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Framework for Dependency-Injection for hosting in WCF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("orthros")]
 [assembly: AssemblyProduct("ExtCF.Hosting")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
